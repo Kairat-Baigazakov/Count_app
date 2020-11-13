5e399bebfaba7106312a051631e956e0651e216a
CompKg <kairat.kg.kk@gmail.com>
2020-11-13 19:03:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/count_app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.count_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static int count = 0;
+    private static final String KEY = "Number";
+
+    public void increment(View view) {
+        count++;
+        display(count);
+    }
+
+    private void display(int count) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("" + String.format("%04d", count));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        TextView textView = findViewById(R.id.textView);
+        outState.putInt(KEY, count);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        int textViewText = savedInstanceState.getInt(KEY);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(String.format("%04d", textViewText));
+    }
+}
